191e48050c91181318b292f356f4c5609377757e
macbialasek <mbialasek@gmail.com>
2015-03-24 20:05:41 +0100

    added QDA

--- model_LDA.R
+++ model_LDA.R
@@ -37,6 +37,15 @@ lda.fit <- lda(target~., data=dataTrain)
 lda.prediction <- predict(lda.fit, newdata=dataTest, type="prob")
 submit[,2:10] <- predict(lda.fit, newdata=dataTest, type="prob")$posterior
 
-write.csv(submit, 'submit.csv', quote=FALSE, row.names = FALSE)
+write.csv(submit, 'lda.submit.csv', quote=FALSE, row.names = FALSE)
 
-#1.05 on benchmark in Kaggle
+#1.029 on benchmark in Kaggle
+#Quick check on QDA
+qda.fit <- qda(target~.,data=dataTrain)
+qda.prediction <- predict(qda.fit, newdata=dataTest, type="prob")
+qda.submit <- submit
+qda.submit[,2:10] <- predict(qda.fit, newdata=dataTest, type="prob")$posterior
+write.csv(qda.submit, 'qda.submit.csv', quote=FALSE, row.names = FALSE)
+#...which gives 6 on benchmark... lot, lot worse
+
+#tuning of LDA???
