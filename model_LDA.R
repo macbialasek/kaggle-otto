2c8de19c8dbda2bdc569f0e17138a81612a5c887
macbialasek <mbialasek@gmail.com>
2015-03-24 19:37:14 +0100

    Final LDA model

--- model_LDA.R
+++ model_LDA.R
@@ -7,9 +7,11 @@ setwd("C:/Users/maciek/Desktop/Studia dok/Courses/kaggle-otto/data")
 library(dplyr)
 library(MASS)
 
-# Read train data:
+# Read train data, test data, submission file:
 
-dataTrain <- read.csv("train.csv")
+dataTrain <- read.csv("train.csv")[,-1]
+dataTest <- read.csv("test.csv")[,-1]
+submit <- read.csv("sampleSubmission.csv")
 str(dataTrain)
 
 # Sample data:
@@ -20,9 +22,20 @@ dataTestSample <- dataTrain[sample(nrow(dataTrain), 1000), ]
 
 #LDA model
 
-lda.fit <- lda(target~., data=dataTrainSample)
+lda.fit <- lda(target~., data=dataTrain)
 lda.fit
-lda.pred <- predict(lda.fit,dataTestSample)
+lda.pred <- predict(lda.fit,dataTestSample,type="prob")
 data.frame(lda.pred)[1:5,]
 table(lda.pred$class,dataTestSample$target)
 mean(lda.pred$class==dataTestSample$target)
+
+#test for submission (I don't know, if posterior means probabilities...
+
+predicted <- predict(lda.fit, newdata=dataTest, type="prob")$class
+
+lda.fit <- lda(target~., data=dataTrain)
+lda.prediction <- predict(lda.fit, newdata=dataTest, type="prob")
+submit[,2:10] <- predict(lda.fit, newdata=dataTest, type="prob")$posterior
+
+write.csv(submit, 'submit.csv', quote=FALSE, row.names = FALSE)
+
